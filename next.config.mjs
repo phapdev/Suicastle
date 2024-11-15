5c661c4578cc328c4cbe7a49c077f55d2bb45acf
phapdev <luongphap1810@gmail.com>
2024-11-16 00:42:11 +0700

    hotfix: google client ios

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const getGoogleClientId = () => {
-  if (navigator.userAgent.match(/iPhone/i)) {
-    return process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID_IOS;
-  } else {
-    return process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-  }
-};
+
 
 const nextConfig = {
   reactStrictMode: false,
   env: {
     ENOKI_PUB_KEY: process.env.ENOKI_PUB_KEY,
-    GOOGLE_CLIENT_ID: getGoogleClientId(),
+    GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
   },
 };
 
